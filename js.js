9ef3b18c550f5bdf575b749eb29ee29cac0d796d
Vladan <vladanvelojic@gmail.com>
2018-01-29 12:31:11 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#typed', {
+    strings: ["dynamic websites", "landing pages", "web applications..."],
+    typeSpeed: 60,
+    smartBackspace: true,
+    backSpeed: 20,
+    loop: true,
+    loopCount: Infinity,
+});
+
+var angleDown = document.getElementById('angle-down');
+var myWork = document.getElementsByClassName('section-two');
+
+// function scrollDown(){
+    
+// };
